640dbf33ea5565c1637de0d70123ad70bfc92163
Charles-André  Beaudry <charles-andre.beaudry.1@ulaval.ca>
2018-02-03 19:48:05 -0500

    Everything works

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,76 @@
 import org.chocosolver.solver.Model;
 import org.chocosolver.solver.Solution;
+import org.chocosolver.solver.Solver;
+import org.chocosolver.solver.constraints.extension.Tuples;
 import org.chocosolver.solver.variables.IntVar;
 
+import java.util.Arrays;
+
 public class Main {
+    private static final int NB_SOLIDS = 4;
+    private static final int NB_FACES_DISPLAYED = 4;
+    private static final int NB_FACES = 6;
+    // 0 = blue, 1 = green, 2 = red, 3 = yellow
+    private static final int[][] SOLIDS = {
+            {3, 2, 1, 2, 0, 1},
+            {1, 2, 3, 0, 0, 1},
+            {0, 3, 3, 1, 3, 2},
+            {3, 2, 0, 3, 1, 2},
+    };
 
     public static void main(String[] args) {
-        int n = 8;
-        Model model = new Model(n + "-queens problem");
-        IntVar[] vars = new IntVar[n];
-        for(int q = 0; q < n; q++){
-            vars[q] = model.intVar("Q_"+q, 1, n);
-        }
-        for(int i  = 0; i < n-1; i++){
-            for(int j = i + 1; j < n; j++){
-                model.arithm(vars[i], "!=",vars[j]).post();
-                model.arithm(vars[i], "!=", vars[j], "-", j - i).post();
-                model.arithm(vars[i], "!=", vars[j], "+", j - i).post();
+        Model model = new Model("Quatre cubes");
+
+        IntVar[][] visibleFaces = model.intVarMatrix(NB_SOLIDS, NB_FACES_DISPLAYED, 0, NB_FACES - 1);
+        IntVar[][] visibleColors = model.intVarMatrix(NB_SOLIDS, NB_FACES_DISPLAYED, 0, NB_FACES_DISPLAYED - 1);
+
+        IntVar[][] visibleFacesTransposed = new IntVar[NB_SOLIDS][NB_FACES_DISPLAYED];
+        IntVar[][] visibleColorsTransposed = new IntVar[NB_SOLIDS][NB_FACES_DISPLAYED];
+
+        for (int i = 0; i < NB_SOLIDS; i++) {
+            for (int j = 0; j < NB_FACES_DISPLAYED; j++) {
+                visibleFacesTransposed[j][i] = visibleFaces[i][j];
+                visibleColorsTransposed[j][i] = visibleColors[i][j];
             }
         }
-        Solution solution = model.getSolver().findSolution();
-        if(solution != null){
-            System.out.println(solution.toString());
+
+        for (int i = 0; i < NB_SOLIDS; i++) {
+            Tuples tuples = new Tuples();
+
+            for (int j = 0; j < NB_FACES; j++) {
+                tuples.add(j, SOLIDS[i][j]);
+            }
+
+            for (int j = 0; j < NB_FACES_DISPLAYED; j++) {
+                model.table(visibleFaces[i][j], visibleColors[i][j], tuples).post();
+            }
+
+            for (int j = 0; j < NB_FACES_DISPLAYED / 2; j++) {
+                model.arithm(visibleFaces[i][j], "+", visibleFaces[i][j + 2], "=", 5).post();
+            }
+
+            model.allDifferent(visibleFaces[i]).post();
+            model.allDifferent(visibleColorsTransposed[i]).post();
         }
+
+        Solver solver = model.getSolver();
+        Solution solution = solver.findSolution();
+
+        if (solution != null) {
+            Arrays.stream(visibleFacesTransposed).forEach(solid -> {
+                Arrays.stream(solid).forEach(face -> System.out.printf("%s ", face.getValue()));
+                System.out.println();
+            });
+
+            System.out.println();
+
+            Arrays.stream(visibleColorsTransposed).forEach(solid -> {
+                Arrays.stream(solid).forEach(color -> System.out.printf("%s ", new char[]{'B', 'G', 'R', 'Y'}[color.getValue()]));
+                System.out.println();
+            });
+            System.out.println();
+        }
+
+        solver.printStatistics();
     }
 }

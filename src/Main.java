dcbc0ad3b9e10bba84b8e3adadc8787b01046286
Charles-André  Beaudry <charles-andre.beaudry.1@ulaval.ca>
2018-02-07 14:44:47 -0500

    Clearer code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import org.chocosolver.solver.Model;
-import org.chocosolver.solver.Solution;
 import org.chocosolver.solver.Solver;
 import org.chocosolver.solver.constraints.extension.Tuples;
 import org.chocosolver.solver.variables.IntVar;
@@ -7,69 +6,93 @@ import org.chocosolver.solver.variables.IntVar;
 import java.util.Arrays;
 
 public class Main {
+    enum Color {
+        BLUE(0),
+        GREEN(1),
+        RED(2),
+        YELLOW(3);
+
+        private final int value;
+
+        Color(int value) {
+            this.value = value;
+        }
+    }
+
     private static final int NB_SOLIDS = 4;
     private static final int NB_FACES_DISPLAYED = 4;
+    private static final int OPPOSITE_FACE_DISTANCE = NB_FACES_DISPLAYED / 2;
     private static final int NB_FACES = 6;
-    // 0 = blue, 1 = green, 2 = red, 3 = yellow
-    private static final int[][] SOLIDS = {
-            {3, 2, 1, 2, 0, 1},
-            {1, 2, 3, 0, 0, 1},
-            {0, 3, 3, 1, 3, 2},
-            {3, 2, 0, 3, 1, 2},
+    private static final int OPPOSITE_FACES_SUM = NB_FACES - 1;
+    private static final Color[][] SOLIDS_COLORS = {
+            {Color.YELLOW, Color.RED, Color.GREEN, Color.RED, Color.BLUE, Color.GREEN},
+            {Color.GREEN, Color.RED, Color.YELLOW, Color.BLUE, Color.BLUE, Color.GREEN},
+            {Color.BLUE, Color.YELLOW, Color.YELLOW, Color.GREEN, Color.YELLOW, Color.RED},
+            {Color.YELLOW, Color.RED, Color.BLUE, Color.YELLOW, Color.GREEN, Color.RED},
     };
+
+    static class Face {
+        final IntVar number;
+        final IntVar color;
+
+        Face(IntVar number, IntVar color) {
+            this.number = number;
+            this.color = color;
+        }
+    }
 
     public static void main(String[] args) {
         Model model = new Model("Quatre cubes");
 
-        IntVar[][] visibleFaces = model.intVarMatrix(NB_SOLIDS, NB_FACES_DISPLAYED, 0, NB_FACES - 1);
-        IntVar[][] visibleColors = model.intVarMatrix(NB_SOLIDS, NB_FACES_DISPLAYED, 0, NB_FACES_DISPLAYED - 1);
+        Face[][] visibleFaces = new Face[NB_SOLIDS][NB_FACES_DISPLAYED];
+        for (Face[] solid : visibleFaces) {
+            Arrays.setAll(solid, face -> new Face(
+                    model.intVar(0, NB_FACES - 1),
+                    model.intVar(0, NB_FACES_DISPLAYED - 1)
+            ));
+        }
 
-        IntVar[][] visibleFacesTransposed = new IntVar[NB_SOLIDS][NB_FACES_DISPLAYED];
-        IntVar[][] visibleColorsTransposed = new IntVar[NB_SOLIDS][NB_FACES_DISPLAYED];
-
-        for (int i = 0; i < NB_SOLIDS; i++) {
-            for (int j = 0; j < NB_FACES_DISPLAYED; j++) {
-                visibleFacesTransposed[j][i] = visibleFaces[i][j];
-                visibleColorsTransposed[j][i] = visibleColors[i][j];
+        Face[][] visibleFacesTransposed = new Face[NB_FACES_DISPLAYED][NB_SOLIDS];
+        for (int noSolid = 0; noSolid < NB_SOLIDS; noSolid++) {
+            for (int noFace = 0; noFace < NB_FACES_DISPLAYED; noFace++) {
+                visibleFacesTransposed[noFace][noSolid] = visibleFaces[noSolid][noFace];
             }
         }
 
-        for (int i = 0; i < NB_SOLIDS; i++) {
+        for (int noSolid = 0; noSolid < NB_SOLIDS; noSolid++) {
             Tuples tuples = new Tuples();
 
-            for (int j = 0; j < NB_FACES; j++) {
-                tuples.add(j, SOLIDS[i][j]);
+            for (int noFaceOfSolid = 0; noFaceOfSolid < NB_FACES; noFaceOfSolid++) {
+                tuples.add(noFaceOfSolid, SOLIDS_COLORS[noSolid][noFaceOfSolid].value);
             }
 
-            for (int j = 0; j < NB_FACES_DISPLAYED; j++) {
-                model.table(visibleFaces[i][j], visibleColors[i][j], tuples).post();
+            for (int noFaceDisplayed = 0; noFaceDisplayed < NB_FACES_DISPLAYED; noFaceDisplayed++) {
+                model.table(visibleFaces[noSolid][noFaceDisplayed].number, visibleFaces[noSolid][noFaceDisplayed].color, tuples).post();
             }
 
-            for (int j = 0; j < NB_FACES_DISPLAYED / 2; j++) {
-                model.arithm(visibleFaces[i][j], "+", visibleFaces[i][j + 2], "=", 5).post();
+            for (int noFaceDisplayed = 0; noFaceDisplayed < OPPOSITE_FACE_DISTANCE; noFaceDisplayed++) {
+                model.arithm(visibleFaces[noSolid][noFaceDisplayed].number, "+", visibleFaces[noSolid][noFaceDisplayed + OPPOSITE_FACE_DISTANCE].number, "=", OPPOSITE_FACES_SUM).post();
             }
 
-            model.allDifferent(visibleFaces[i]).post();
-            model.allDifferent(visibleColorsTransposed[i]).post();
+            model.allDifferent(Arrays.stream(visibleFaces[noSolid]).map(face -> face.number).toArray(IntVar[]::new)).post();
+            model.allDifferent(Arrays.stream(visibleFacesTransposed[noSolid]).map(face -> face.color).toArray(IntVar[]::new)).post();
         }
 
         Solver solver = model.getSolver();
-        Solution solution = solver.findSolution();
+        solver.findSolution();
 
-        if (solution != null) {
-            Arrays.stream(visibleFacesTransposed).forEach(solid -> {
-                Arrays.stream(solid).forEach(face -> System.out.printf("%s ", face.getValue()));
-                System.out.println();
-            });
+        for (int noSolid = 0; noSolid < NB_SOLIDS; noSolid++) {
+            System.out.printf("   Cube %s    ", noSolid + 1);
+        }
+        System.out.println();
 
-            System.out.println();
-
-            Arrays.stream(visibleColorsTransposed).forEach(solid -> {
-                Arrays.stream(solid).forEach(color -> System.out.printf("%s ", new char[]{'B', 'G', 'R', 'Y'}[color.getValue()]));
-                System.out.println();
-            });
+        for (Face[] line : visibleFacesTransposed) {
+            for (Face face : line) {
+                System.out.printf("|%-6s (%s)| ", Color.values()[face.color.getValue()], face.number.getValue() + 1);
+            }
             System.out.println();
         }
+        System.out.println();
 
         solver.printStatistics();
     }
